9514082aeaa21f155adc95ffd01f3b57e12c9922
shno-24sudo <shno24@my.yorku.ca>
2026-03-29 17:39:59 -0400

    Add grayscale image processing in Grayscale class
    Implemented the image processing logic to convert an input image to grayscale by averaging the RGB channels.

--- source/Grayscale.java
+++ source/Grayscale.java
@@ -3,12 +3,19 @@ public class Grayscale extends Converter {
 
     @Override
     protected void processImage(java.awt.image.BufferedImage inputImage, java.awt.image.BufferedImage outputImage) {
-        // convert the image to particular ARGB value
-    }
+        int width = inputImage.getWidth();
+        int height = inputImage.getHeight();
 
-    public void setRGB(int x, int y, int rgb) {
-        x = getWidth();
-        y = getHeight();
-        //rgb = toInt();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                ARGB argb = new ARGB(inputImage.getRGB(x, y));
+
+                // Average the three channels to produce a neutral gray
+                int gray = (argb.red + argb.green + argb.blue) / 3;
+
+                ARGB grayPixel = new ARGB(argb.alpha, gray, gray, gray);
+                outputImage.setRGB(x, y, grayPixel.toInt());
+            }
+        }
     }
 }
