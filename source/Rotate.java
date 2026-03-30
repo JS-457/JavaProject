324c31b5cea95b36a02bf9bdfb65563880133014
King Galactix <sayedjamshidshah@gmail.com>
2026-03-29 20:39:49 -0400

    Implemented rotate values.
    - Implemented the rotation of the image to 90 degrees clockwise.
    - Outputs.

--- source/Rotate.java
+++ source/Rotate.java
@@ -1,7 +1,23 @@
 
 public class Rotate extends Converter {
 
+    @Override
     protected void processImage(java.awt.image.BufferedImage inputImage, java.awt.image.BufferedImage outputImage) {
-        // convert the image to particular ARGB value
+        int width = inputImage.getWidth();
+        int height = inputImage.getHeight();
+
+        // Rotating the image to 90 degrees clockwise
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int pixel = inputImage.getRGB(x, y);
+
+                // Rotates the X and Y coordinates by 90 degrees
+                int rotX = height - 1 - x;
+                int rotY = x;
+
+                // Output image now has the new XY coordinates and pixel values.
+                outputImage.setRGB(rotX, rotY, pixel);
+            }
+        }
     }
 }
