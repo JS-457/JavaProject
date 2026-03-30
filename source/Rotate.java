3a686d0f53846d44af07321180173a91ece8436f
King Galactix <sayedjamshidshah@gmail.com>
2026-03-29 21:39:41 -0400

    Fully implemented Rotate.java
    This should work as intended and rotate clockwise by 90 degrees. Also accounts for image cropping and out of bounds states.

--- source/Rotate.java
+++ source/Rotate.java
@@ -6,17 +6,27 @@ public class Rotate extends Converter {
         int width = inputImage.getWidth();
         int height = inputImage.getHeight();
 
+        int outWidth = outputImage.getWidth();
+        int outHeight = outputImage.getHeight();
+
         // Rotating the image to 90 degrees clockwise
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 int pixel = inputImage.getRGB(x, y);
 
                 // Rotates the X and Y coordinates by 90 degrees
-                int rotX = height - 1 - x;
+                int rotX = height - 1 - y;
                 int rotY = x;
 
+                // Initially, the image would crop a lot if it wasn't a perfect square
+                // So these centerX/Y variables are used to truly  adjust the rotation of the image's width and height.
+                int centerX = rotX + (outWidth - height) / 2;
+                int centerY = rotY + (outHeight - width) / 2;
+
                 // Output image now has the new X/Y coordinates and pixel values.
-                outputImage.setRGB(rotX, rotY, pixel);
+                if (centerX >= 0 && centerX < outWidth && centerY >= 0 && centerY < outHeight) {
+                    outputImage.setRGB(centerX, centerY, pixel);
+                }
             }
         }
     }
